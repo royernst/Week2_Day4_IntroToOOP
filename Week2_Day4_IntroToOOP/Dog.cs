c146df81557de4c23787070c1fdb412e4de65f04
royernst <roy.e31@hotmail.com>
2017-10-09 11:42:34 -0400

    Dog class created

--- Week2_Day4_IntroToOOP/Dog.cs
+++ Week2_Day4_IntroToOOP/Dog.cs
@@ -8,5 +8,36 @@ namespace Week2_Day4_IntroToOOP
 {
     class Dog
     {
+        //fields
+        private string furLength;
+        private int weight;
+        private string runningSpeed;
+        private float height;
+
+        //Properties
+        public string FurLength
+        {
+            get { return this.furLength; }
+            set { this.furLength = value; }
+        }
+
+        public float Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public string RunningSpeed
+        {
+            get { return this.runningSpeed; }
+            set { this.runningSpeed = value; }
+        }
+
+        public int Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
     }
 }
